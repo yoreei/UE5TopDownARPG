3fbb99ee4242feb5f41232251af7848801f9eed8
Yordan Grigorov <oddnecessity@gmail.com>
2024-01-19 01:34:25 +0200

    wip all

--- Source/UE5TopDownARPG/UE5TopDownARPGPlayerController.cpp
+++ Source/UE5TopDownARPG/UE5TopDownARPGPlayerController.cpp
@@ -12,6 +12,216 @@
 #include "EnhancedInputSubsystems.h"
 #include "UE5TopDownARPG.h"
 #include "NavigationSystem.h"
+#include <map>
+
+struct IntegrationField {
+	int MapWidth;
+	int MapHeight;
+
+	float IntegratedCost;
+	uint8_t ActiveWaveFront : 4;
+	uint8_t LOS : 4;
+
+
+};
+
+enum class EDirection : uint8_t {
+	North = 0,
+	NorthEast,
+	East,
+	SouthEast,
+	South,
+	SouthWest,
+	West,
+	NorthWest
+};
+
+struct FlowField {
+	EDirection DirectionLookup : 4;
+	uint8_t Pathable : 2;
+	uint8_t LOS : 2;
+};
+
+void DebugDraw(UWorld* World, int MapWidth, int MapHeight, const TArray<uint8_t>& CostFields)
+{
+	FVector RayStart;
+	FVector RayEnd;
+	for (int i = 0; i <= MapHeight; ++i)
+	{
+		for (int j = 0; j <= MapWidth; ++j)
+		{
+			RayStart = { i * 100.f + 50, j * 100.f + 50, 300.f };
+			RayEnd = { i * 100.f + 50, j * 100.f + 50, 100.f };
+			DrawDebugDirectionalArrow(World, RayStart, RayEnd, 3.0f, CostFields[i * MapWidth + j] == 255 ? FColor::Red : FColor::Green, true, 9999999.f, 0, 2.f);
+		}
+	}
+}
+
+void DebugDraw(UWorld* World, int MapWidth, int MapHeight, const TArray<IntegrationField> IntegrationFields)
+{
+	FVector RayStart;
+	FVector RayEnd;
+	for (int i = 0; i <= MapHeight; ++i)
+	{
+		for (int j = 0; j <= MapWidth; ++j)
+		{
+			RayStart = { i * 100.f + 50, j * 100.f + 50, 300.f };
+			RayEnd = { i * 100.f + 50, j * 100.f + 50, 100.f };
+			DrawDebugDirectionalArrow(World, RayStart, RayEnd, 3.0f, IntegrationFields[i * MapWidth + j].LOS == false ? FColor::Red : FColor::Green, true, 9999999.f, 0, 2.f);
+		}
+	}
+}
+
+void DebugDraw(UWorld* World, int MapWidth, int MapHeight, const TArray<FlowField>& FlowFields)
+{
+	FVector RayStart;
+	FVector RayEnd;
+
+	std::map<EDirection, FVector2D> directionOffsets =
+	{
+		{EDirection::North, FVector2D(0, 25)},
+		{EDirection::NorthEast, FVector2D(25, 25)},
+		{EDirection::East, FVector2D(25, 0)},
+		{EDirection::SouthEast, FVector2D(25, -25)},
+		{EDirection::South, FVector2D(0, -25)},
+		{EDirection::SouthWest, FVector2D(-25, -25)},
+		{EDirection::West, FVector2D(-25, 0)},
+		{EDirection::NorthWest, FVector2D(-25, -25)},
+	};
+	FVector2D offset;
+
+	for (int i = 0; i <= MapHeight; ++i)
+	{
+		for (int j = 0; j <= MapWidth; ++j)
+		{
+			offset = directionOffsets[FlowFields[i * MapWidth + j].DirectionLookup];
+			RayStart = { i * 100.f + 50 - offset.X * 25, j * 100.f + 50 - offset.Y * 25, 50.f };
+			RayEnd = { i * 100.f + 50 + offset.X * 25, j * 100.f + 50 + offset.Y * 25, 50.f };
+
+			DrawDebugDirectionalArrow(World, RayStart, RayEnd, 3.f, FColor::Green, true, 9999999.f, 0, 2.f);
+		}
+	}
+}
+
+/*
+Direction is normalized
+*/
+void BresenhamsRay2D(int MapHeight, int MapWidth, const TArray<uint8_t>& CostFields, const FVector2D& Direction, int PosX, int PosY, TArray<IntegrationField>& IntegrationFields)
+{
+	int x = PosX;
+	int y = PosY;
+	int endX = std::floor(PosX + Direction.X * MapWidth);
+	int endY = std::floor(PosY + Direction.Y * MapHeight);
+
+	int dx = std::abs(endX - x);
+	int dy = -std::abs(endY - y);
+	int sx = x < endX ? 1 : -1;
+	int sy = y < endY ? 1 : -1;
+	int err = dx + dy, e2;
+
+	while (true) {
+		if (x >= 0 && x < MapWidth && y >= 0 && y < MapHeight) {
+			if (CostFields[y * MapWidth + x] == 255) {
+				IntegrationFields[y * MapWidth + x].LOS = false;
+				break;
+			}
+			else {
+				IntegrationFields[y * MapWidth + x].LOS = true;
+			}
+		}
+		else {
+			// Break if we are out of bounds
+			break;
+		}
+
+		if (x == endX && y == endY) break;
+		e2 = 2 * err;
+		if (e2 >= dy) { err += dy; x += sx; }
+		if (e2 <= dx) { err += dx; y += sy; }
+	}
+}
+
+void LineOfSightPass(int MapHeight, int MapWidth, const FVector& Goal, OUT TArray<IntegrationField>& IntegrationFields)
+{
+
+}
+
+void CalculateIntegrationFields(int MapHeight, int MapWidth, const FVector& Goal, OUT TArray<IntegrationField>& IntegrationFields)
+{
+	LineOfSightPass();
+
+}
+
+void CalculateFlowFields(int MapHeight, int MapWidth, const FVector& EndPos, OUT TArray<FlowField> FlowFields)
+{
+
+
+}
+
+bool CalculateCostFields(UWorld* World, int MapHeight, int MapWidth, OUT TArray<uint8_t>& CostFields)
+{
+	if (IsValid(World))
+	{
+		return false;
+	}
+
+	FVector HitLocation;
+	FVector RayStart;
+	FVector RayEnd;
+	FHitResult OutHit;
+	for (int i = 0; i <= MapHeight; ++i)
+	{
+		for (int j = 0; j <= MapWidth; ++j)
+		{
+			RayStart = FVector(i * 100 + 50, j * 100 + 50, 300);
+			RayEnd = FVector(i * 100 + 50, j * 100 + 50, 100);
+			FCollisionQueryParams CollisionQueryParams(SCENE_QUERY_STAT(ClickableTrace), true);
+			bool bLineTraceObstructed = World->LineTraceSingleByChannel(OutHit, RayStart, RayEnd, ECollisionChannel::ECC_WorldStatic /* , = FCollisionQueryParams::DefaultQueryParam, = FCollisionResponseParams::DefaultResponseParam */);
+
+			CostFields[i * MapHeight + j] = bLineTraceObstructed ? 1 : 255;
+			UE_LOG(LogUE5TopDownARPG, Log, TEXT("Raycast at %s, %s: %d"), *(RayStart.ToString()), *(RayEnd.ToString()), (int)bLineTraceObstructed);
+			DrawDebugDirectionalArrow(World, RayStart, RayEnd, 3.0f, bLineTraceObstructed ? FColor::Red : FColor::Green, true, 9999999.f, 0, 2.f);
+		}
+	}
+	return true;
+}
+
+void DoFlowTiles(UWorld* World)
+{
+	if (IsValid(World) == false)
+	{
+		return;
+	}
+
+	int MapHeight = 30;
+	int MapWidth = 30;
+	TArray<uint8_t> CostFields;
+	CostFields.Init(1, MapHeight * MapWidth);
+	CalculateCostFields(World, MapHeight, MapWidth, CostFields);
+
+	FVector Goal{ 27.f, 27.f, 1.f };
+	TArray<IntegrationField> IntegrationFields;
+	IntegrationFields.Init({ 1, 0, 0 }, MapHeight * MapWidth);
+	CalculateIntegrationFields(MapHeight, MapWidth, Goal, IntegrationFields);
+
+	TArray<FlowField> FlowFields;
+	FlowFields.Init({ EDirection::North, 0, 0 }, MapHeight * MapWidth);
+	CalculateFlowFields(MapHeight, MapWidth, Goal, FlowFields);
+}
+
+// TESTS
+
+bool Test_BresenhamsRay2D(UWorld* World)
+{
+	return false;
+}
+
+void MainFlowTiles(UWorld* World)
+{
+	// DoFlowTiles(World);
+	Test_BresenhamsRay2D(World);
+}
+
 
 AUE5TopDownARPGPlayerController::AUE5TopDownARPGPlayerController()
 {
@@ -47,38 +257,7 @@ void AUE5TopDownARPGPlayerController::BeginPlay()
 		return;
 	}
 
-	UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(World);
-
-	if (IsValid(NavSys) == false)
-	{
-		return;
-	}
-	ANavigationData* NavData = NavSys->GetDefaultNavDataInstance();
-
-	if (IsValid(NavData) == false)
-	{
-		return;
-	}
-
-	FVector HitLocation;
-	FVector RayStart;
-	FVector RayEnd;
-	FHitResult OutHit;
-
-	for (int i = 0; i <= 30; i++)
-	{
-		for (int j = 0; j <= 30; j++)
-		{
-			RayStart = FVector(i * 100 + 50, j * 100 + 50, 300);
-			RayEnd = FVector(i * 100 + 50, j * 100 + 50, 100);
-			FCollisionQueryParams CollisionQueryParams(SCENE_QUERY_STAT(ClickableTrace), true);
-			bool bLineTraceObstructed = World->LineTraceSingleByChannel(OutHit, RayStart, RayEnd, ECollisionChannel::ECC_WorldStatic /* , = FCollisionQueryParams::DefaultQueryParam, = FCollisionResponseParams::DefaultResponseParam */);
-			
-			UE_LOG(LogUE5TopDownARPG, Log, TEXT("Raycast at %s, %s: %d"), *(RayStart.ToString()), *(RayEnd.ToString()), (int)bLineTraceObstructed);
-			DrawDebugDirectionalArrow(World, RayStart, RayEnd, 3.0f, bLineTraceObstructed ? FColor::Red : FColor::Green, true, 9999999.f, 0, 2.f);
-		}
-	}
-
+	MainFlowTiles(World);
 }
 
 void AUE5TopDownARPGPlayerController::SetupInputComponent()

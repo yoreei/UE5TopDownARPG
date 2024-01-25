06f5f1f1c50311956ad6962c599571cdf738d2f7
Yordan Grigorov <oddnecessity@gmail.com>
2024-01-25 11:38:31 +0200

    broke sln generation

---
+++ Source/UE5TopDownARPG/CrowdPF/CrowdNavData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavigationData.h"
+#include "CrowdNavData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5TOPDOWNARPG_API ACrowdNavData : public ANavigationData
+{
+	GENERATED_BODY()
+	
+};
